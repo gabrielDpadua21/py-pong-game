9eb0996c2ac09f36bc2ff99d338c2d4ada64f913
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2023-06-20 22:37:10 -0300

    feat: ✨ create paddles and move

---
+++ classes/paddle.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+SHAPE = "square"
+WIDTH = 5
+LEN = 1
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.penup()
+        self.goto(position)
+        self.shape(SHAPE)
+        self.shapesize(stretch_wid=WIDTH, stretch_len=LEN)
+        self.color("white")
+
+
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from turtle import Turtle, Screen
+from classes.paddle import Paddle
 
-
-turtle = Turtle()
 
 if __name__ == "__main__":
     screen = Screen()
     screen.setup(width=800, height=600)
     screen.bgcolor("black")
     screen.title("Pong Game")
-    
+    screen.listen()
+
+    paddle01 = Paddle((350, 0))
+    paddle02 = Paddle((-350, 0))
+
+    screen.onkey(paddle01.go_up, "w")
+    screen.onkey(paddle01.go_down, "s")
+    screen.onkey(paddle02.go_up, "Up")
+    screen.onkey(paddle02.go_down, "Down")
+
     screen.exitonclick()
     
