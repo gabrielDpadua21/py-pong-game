6c9acf4eb3fc6b9155faee8c663fa51439100b53
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2023-06-20 22:16:35 -0300

    feat: ✨ setup screen

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from turtle import Turtle, Screen
 
+
+turtle = Turtle()
 
 if __name__ == "__main__":
-    print("Template python")
+    screen = Screen()
+    screen.setup(width=800, height=600)
+    screen.bgcolor("black")
+    screen.title("Pong Game")
+    
+    screen.exitonclick()
     
